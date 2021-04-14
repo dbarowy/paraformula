bc463a6f3504835c6f0dbb558a83550bf0484fe3
Daniel W. Barowy <dbarowy@cs.williams.edu>
2021-04-14 17:17:32 -0400

    Fix paraformula main path

--- examples/simple/src/simple.ts
+++ examples/simple/src/simple.ts
@@ -1,6 +1,9 @@
 import { Paraformula } from "paraformula";
+import { Primitives as P, CharUtil as CU } from "parsecco";
 
 console.time("total");
 const input = "=SUM(A1,B2:B77,5)";
-const output = Paraformula.parse(input);
+const p = P.str("=SUM");
+// const output = Paraformula.parse(input);
+const output = p(new CU.CharStream(input));
 console.timeEnd("total");

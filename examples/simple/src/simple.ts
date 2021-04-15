8b3d20e0244ba1b15669a00e24b4a81c7e5e743b
Daniel W. Barowy <dbarowy@cs.williams.edu>
2021-04-15 09:06:05 -0400

    Totally just stabbing in the dark as to whether this fixes 'is not a function' errors for paraformula package consumers

--- examples/simple/src/simple.ts
+++ examples/simple/src/simple.ts
@@ -1,7 +1,10 @@
 import { Paraformula } from "paraformula";
+// import * as Paraformula from "paraformula";
+// const Paraformula = require("paraformula");
+// import Paraformula from "paraformula";
+// import { default as Paraformula } from "paraformula";
+// import { Primitives as P, CharUtil as CU } from "parsecco";
 
-console.time("total");
 const input = "=SUM(A1,B2:B77,5)";
-const output = Paraformula.parse(input);
-console.log(output);
-console.timeEnd("total");
+const output2 = Paraformula.parse(input);
+console.log(output2);
